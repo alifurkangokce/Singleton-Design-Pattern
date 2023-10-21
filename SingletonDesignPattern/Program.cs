4177e79dfa1885aa6973681a56016fa455bf8335
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2023-10-21 13:23:18 +0300

    Add project files.

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using SingletonDesignPattern;
+
+Singleton singleton1 = Singleton.Instance;
+
+// İkinci bir örnek almaya çalışırsak, aynı örnek kullanılacaktır.
+Singleton singleton2 = Singleton.Instance;
+
+Console.WriteLine("singleton1 ve singleton2 aynı örnek mi? " + (singleton1 == singleton2));
+
+// Singleton sınıfının metodu çağrılabilir.
+singleton1.SomeMethod();
+
+Console.ReadLine();
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+            // Singleton sınıfının yapıcı metodu özel olarak işaretlendi,
+            // böylece sınıf dışında yeni örnekler oluşturulamaz.
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                            Console.WriteLine("Singleton örneği oluşturuldu.");
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void SomeMethod()
+        {
+            Console.WriteLine("Singleton sınıfı metodu çağrıldı.");
+        }
+    }
+}
